e98388b37e2cffd38211eb8e3ca49a60cd1091c7
= <rossgardiner24@gmail.com>
2023-07-26 15:18:47 +0100

    GREY -> GRAY

--- train_models.sh
+++ train_models.sh
@@ -6,7 +6,7 @@ for((i = 0; i < ${#MODEL_NAMES[@]}; i++)); do
         for ((k = 0; k < ${#USE_GREYSCALE[@]}; k++)); do
 
             python /tf/models/research/slim/train_image_classifier.py \
-                --train_dir="${TRAINING_DIR}/${TRAINING_NAME}_${MODEL_NAMES[i]}_${IMAGE_SIZES[j]}_${USE_GREYSCALE[k]}" \
+                --train_dir="${TRAINING_DIR}/${TRAINING_NAME}_${MODEL_NAMES[i]}_${IMAGE_SIZES[j]}_${USE_GRAYSCALE[k]}" \
                 --dataset_name=visualwakewords \
                 --dataset_split_name="${TRAINING_NAME}" \
                 --dataset_dir="${DATA_DIR}"  \
@@ -20,7 +20,7 @@ for((i = 0; i < ${#MODEL_NAMES[@]}; i++)); do
                 --num_epochs_per_decay=${NUM_EPOCHS_PER_DECAY} \
                 --moving_average_decay=${MOVING_AVERAGE_DECAY} \
                 --batch_size=${BATCH_SIZE} \
-                --max_number_of_steps=${MAX_NUMBER_OF_STEPS} > "${TRAINING_NAME}_${MODEL_NAMES[i]}_${IMAGE_SIZES[j]}_${USE_GREYSCALE[k]}_output.log" 2>&1
+                --max_number_of_steps=${MAX_NUMBER_OF_STEPS} > "${TRAINING_NAME}_${MODEL_NAMES[i]}_${IMAGE_SIZES[j]}_${USE_GRAYSCALE[k]}_output.log" 2>&1
         done
     done 
 done
