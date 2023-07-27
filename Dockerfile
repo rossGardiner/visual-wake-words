47350545f63b18c0f889e3ac2676685844093fdb
= <rossgardiner24@gmail.com>
2023-07-27 11:51:52 +0100

    ready for deployment

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM tensorflow/tensorflow:1.15.2-gpu-py3-jupyter
 
 WORKDIR /tf
 COPY train_models.sh /tf/
+RUN ["chmod", "+x", "/tf/train_models.sh"]
 COPY config.cfg /tf/
 
 RUN rm /etc/apt/sources.list.d/cuda.list
@@ -19,3 +20,5 @@ RUN pip3 install --upgrade pip
 RUN pip3 install contextlib2 Pillow tf_slim opencv-python
 
 ENV PYTHONPATH "${PYTHONPATH}:/tf/models/research/slim"
+
+CMD ["/tf/train_models.sh"]
--- train_models.sh
+++ train_models.sh
@@ -1,5 +1,26 @@
+#!/bin/bash
+cleanup() {
+    echo "Stopping the script and cleaning up..."
+    exit 1
+}
+
+trap cleanup SIGINT
 
 source config.cfg
+
+
+#function to map model names to preprocessing names
+mapped_values=()
+
+for value in "${MODEL_NAMES[@]}"; do
+    case "$value" in
+        mobilenet_v1*) mapped_values+=("mobilenet_v1") ;;
+        mobilenet_v2*) mapped_values+=("mobilenet_v2") ;;
+        mobilenet_v3*) mapped_values+=("mobilenet_v3") ;;
+        *) mapped_values+=("$value") ;;
+    esac
+done
+
 
 for((i = 0; i < ${#MODEL_NAMES[@]}; i++)); do
     for((j = 0; j < ${#IMAGE_SIZES[@]}; j++)); do
@@ -11,6 +32,7 @@ for((i = 0; i < ${#MODEL_NAMES[@]}; i++)); do
                 --dataset_split_name="${TRAINING_NAME}" \
                 --dataset_dir="${DATA_DIR}"  \
                 --model_name="${MODEL_NAMES[i]}" \
+                --preprocessing_name=${mapped_values[i]} \
                 --train_image_size=${IMAGE_SIZES[j]} \
                 --use_grayscale=${USE_GRAYSCALE[k]} \
                 --save_summaries_secs=300 \
@@ -20,7 +42,7 @@ for((i = 0; i < ${#MODEL_NAMES[@]}; i++)); do
                 --num_epochs_per_decay=${NUM_EPOCHS_PER_DECAY} \
                 --moving_average_decay=${MOVING_AVERAGE_DECAY} \
                 --batch_size=${BATCH_SIZE} \
-                --max_number_of_steps=${MAX_NUMBER_OF_STEPS} > "${TRAINING_NAME}_${MODEL_NAMES[i]}_${IMAGE_SIZES[j]}_${USE_GRAYSCALE[k]}_output.log" 2>&1
+                --max_number_of_steps=${MAX_NUMBER_OF_STEPS} > "${TRAINING_DIR}/${TRAINING_NAME}_${MODEL_NAMES[i]}_${IMAGE_SIZES[j]}_${USE_GRAYSCALE[k]}_output.log" 2>&1
         done
     done 
 done
